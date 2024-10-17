dd64ad421eb7eac35b3e498b9b450191edeccd74
Bao Huynh <baohuynhgiast1910@gmail.com>
2024-10-17 14:04:49 +0700

    Add abstract leaderboard to test redis

--- server/src/main/java/vn/giabaoblog/giabaoblogserver/data/domains/LeaderBoard.java
+++ server/src/main/java/vn/giabaoblog/giabaoblogserver/data/domains/LeaderBoard.java
@@ -1,6 +1,31 @@
 package vn.giabaoblog.giabaoblogserver.data.domains;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name= "leaderboard")
 public class LeaderBoard {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name= "score_name", nullable = false)
+    public String name;
+
+    @Column(name= "score", nullable = false)
+    public Long score;
+
+    @Column(name= "user_create_id", nullable = false)
+    public String userId;
 
     // add
 
